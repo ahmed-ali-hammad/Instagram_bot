cf872c2eeaaf1f5c603c1fb9bd01f00d640a712a
Ahmed <ahmed.aly.hammad@gmail.com>
2021-04-28 19:06:30 -0400

    the saving functionality was added. next is to configure the options for the webdriver

--- instagram_bot.py
+++ instagram_bot.py
@@ -8,20 +8,32 @@ import time
 import wget
 import os
 
+options =  webdriver.ChromeOptions()
+# options.add_argument("--headless")
 
 class Instagram:
     def __init__(self, keyword, username, password):
         self.keyword  = keyword
         self.username = username
         self.password = password
-        self.driver   = webdriver.Chrome('chromedriver.exe')
+        self.driver   = webdriver.Chrome('chromedriver.exe', options = options)
+
+        #running the search
+        self.login()
+        self.search()
+        self.collect_images()
+        self.search()
+        self.driver.quit()
+
 
     def login(self):
         self.driver.get('https://www.instagram.com/')
 
+        # Explicit  wait for the page to load the element
         username_field = WebDriverWait(self.driver, 10).until(
         EC.presence_of_element_located((By.XPATH, "//input[@name='username']")))
 
+        # Explicit  wait for the page to load the element
         password_field = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, "//input[@name='password']")))
 
@@ -31,38 +43,77 @@ class Instagram:
         password_field.clear() # to make sure the field is empty
         password_field.send_keys(self.password)
 
+        # Explicit  wait for the page to load the element
         login_button = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//button[@type='submit']")))
+                EC.element_to_be_clickable((By.XPATH, "//button[@type='submit']")))
 
         login_button.click()
-        time.sleep(4)
 
-        self.driver.find_element_by_xpath("//button[@class='sqdOP yWX7d    y3zKF     ']").click()
-        time.sleep(4)
+        # Explicit  wait for the page to load the element
+        WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.CSS_SELECTOR, "div.cmbtv button"))).click()
 
-        self.driver.find_element_by_xpath("//button[@class='aOOlW   HoLwm ']").click()
+        # Explicit  wait for the page to load the element
+        WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//button[@class='aOOlW   HoLwm ']"))).click()
 
-        time.sleep(4)
 
     def search(self):
+        # Explicit  wait for the page to load the element
         search_box = WebDriverWait(self.driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search']")))
+            EC.presence_of_element_located((By.XPATH, "//input[@placeholder='Search']")))
 
         search_box.send_keys(self.keyword)
 
-        time.sleep(2)
+        # picking the first choice from the search results
+        WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, "//a[@class='-qQT3'][1]"))).click()
 
-        self.driver.find_element_by_xpath("//a[@class='-qQT3'][1]").click()
+        time.sleep(3)
 
-        time.sleep(5)
+    def save(self, imgs_urls):
+        path = os.getcwd()
+        download_to = os.path.join(path, "downloads")
+        os.mkdir(download_to)
+
+        counter = 0
+        for image in imgs_urls:
+            save_as = os.path.join(download_to,('cat'+ str(counter) +'.jpg'))
+            wget.download(image, save_as)
+            counter = counter + 1
+        print('Data was saved successfully')
 
 
+    def collect_images(self):
+        images = self.driver.find_elements_by_tag_name('img')
+        imgs_urls = [image.get_attribute('src') for image in images]
 
-    def run(self):
-        self.login()
+        len_of_page = 0
+        condition = False
 
+        while condition == False:
 
+            last_count = len_of_page
+
+            # to scroll down we execute this Javascript code
+            len_of_page = self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight); return document.body.scrollHeight;")
+
+            # sleep for 5 secs to give time for the page to load
+            time.sleep(3)
+
+            images_new = self.driver.find_elements_by_tag_name('img')
+
+            # removing the duplicates
+            for image in images_new:
+                if image.get_attribute('src') not in imgs_urls:
+                    imgs_urls.append(image.get_attribute('src'))
+            
+            if last_count == len_of_page :
+                condition = True
+
+        self.save(imgs_urls)
+        # self.driver.quit()
+        
 
 if __name__ == '__main__':
-    search = Instagram("#cats", username, password)
-    search.run()
+    search_item = Instagram("#gjhkg", username, password)
